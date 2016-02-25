93a7dd7548a329db5cd6bca32e8642983f9ce817
Osman Hernandez <osman.sist@gmail.com>
2016-02-25 15:03:43 -0600

    calculating accumulated amount of diseases per week

--- crying-wolf/crying-wolf.js
+++ crying-wolf/crying-wolf.js
@@ -2,13 +2,18 @@ if (Meteor.isClient) {
 
 	Meteor.subscribe('weeks');
 
-	Template.main.created = function () {
+	Template.main.created = function() {
 		this.selectedWeek = new ReactiveVar({});
+		this.pastWeeks = new ReactiveVar([]);
 	};
 
 	Template.main.helpers({
 		weekOptions: () => {
-			return Weeks.find();
+			const weeks = Weeks.find().map((val) => {
+				return val.week;
+			});
+
+			return weeks;
 		},
 		selectedWeek: () => {
 			return Template.instance().selectedWeek.get();
@@ -21,14 +26,53 @@ if (Meteor.isClient) {
 			const week = Weeks.findOne({
 				week: selectedWeek
 			});
+			const pastWeeks = Weeks.find({
+				week: {
+					$lt: selectedWeek
+				}
+			}).map((val) => {
+				return val.diseases;
+			});
 
 			if (week) {
 				template.selectedWeek.set(week);
+				template.pastWeeks.set(pastWeeks);
 			} else {
 				template.selectedWeek.set({});
+				template.pastWeeks.set([]);
 			}
 		},
-		'update-week .week-table': (ev, template, data) => {
+		'update-week .week-table': (ev, template, data, disease) => {
+			const pastWeeks = template.pastWeeks.get();
+			let diseaseUpdate = {};
+			let diseasesGroup = [];
+			let tempDisease = {};
+
+			for (let i = 0; i < pastWeeks.length; i++) {
+				diseasesGroup = diseasesGroup.concat(pastWeeks[i].filter((val) => {
+					return val.disease === disease;
+				}));
+			}
+			if (diseasesGroup.length > 0) {
+				let collectedCurrentYear = 0;
+				let collectedLastYear = 0;
+				for (let j = 0; j < diseasesGroup.length; j++) {
+					collectedCurrentYear += diseasesGroup[j].currentYearAmount;
+					collectedLastYear += diseasesGroup[j].lastYearAmount;
+				}
+				diseaseUpdate = {
+					disease: disease,
+					collectedCurrentYear: collectedCurrentYear,
+					collectedLastYear: collectedLastYear
+				};
+			}
+
+			tempDisease = data.diseases.filter((val) => {
+				return val.disease === disease;
+			})[0];
+
+			tempDisease.collectedCurrentYear = diseaseUpdate.collectedCurrentYear ? diseaseUpdate.collectedCurrentYear + tempDisease.currentYearAmount : tempDisease.currentYearAmount;
+			tempDisease.collectedLastYear = diseaseUpdate.collectedLastYear ? diseaseUpdate.collectedLastYear + tempDisease.lastYearAmount : tempDisease.lastYearAmount;
 			template.selectedWeek.set(data);
 			Meteor.call('updateWeek', data);
 		}
@@ -42,20 +86,20 @@ if (Meteor.isClient) {
 		'input .current-year-amount': (ev, template) => {
 			let value = $(ev.currentTarget).val().trim();
 
-			if(value === '') {
+			if (value === '') {
 				return;
 			}
 			template.data.currentYearAmount = parseInt(value, 10);
-			$('.week-table').trigger('update-week', Template.parentData(1));
+			$('.week-table').trigger('update-week', [Template.parentData(1), template.data.disease]);
 		},
 		'input .last-year-amount': (ev, template) => {
 			let value = $(ev.currentTarget).val().trim();
 
-			if(value === '') {
+			if (value === '') {
 				return;
 			}
 			template.data.lastYearAmount = parseInt(value, 10);
-			$('.week-table').trigger('update-week', Template.parentData(1));
+			$('.week-table').trigger('update-week', [Template.parentData(1), template.data.disease]);
 		}
 	});
 }
